6a6bf84bd1c48215381dd6e3a962489de6a79be6
Nana Yaa <nanayaabob@gmail.com>
2023-02-16 17:00:48 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main -  the entry point.
+ * Return:Always 0 if a success.
+*/
+int main(void)
+
+{
+
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+
+}
