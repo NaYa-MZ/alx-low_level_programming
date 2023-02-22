bf5b83193eb999d00ffe75a8d2ab806979a8ab74
Nana Yaa <nanayaabob@gmail.com>
2023-02-22 08:12:43 -0800

    lowercases

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
-*_islower c - checks for lowercase x'ter*Return: 1 if c is lowercase
-*Return: 0 if c is uppercase
+*_islower  - checks for lowercase x'ter
+*@c: the x'ter to be checked
+*Return: 1 if c is lowercase or 0 if otherwise
 */
 int _islower(int c)
 {
-int c;
-if (c >= 'a' && c >= 'z')
+if (c >= 'a' && c <= 'z')
 return (1);
 else
 return (0);
---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*_isalpha - it is to check if a x'ter is an alphabet
+*@c: is the x'ter to be checked
+*Return: 1 if c is a letter and 0 if otherwise
+*/
+int _isalpha(int c)
+{
+if ((c >= 'a' && c <= 'z') || (c >= 'A
+' && c <= 'Z')); 
+return (1);
+else 
+return (0);
+
+
+
+
+}
