ffca161fbf04347a4b101adba4d6d2792fa517fa
Nana Yaa <nanayaabob@gmail.com>
2023-02-24 09:34:12 -0800

    Finally

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+*main - the main function
+*@i: interger
+*fizz_buzz - prints fizz buzz instead of numbers
+*fizz for multiples of 3
+*buzz for multiples of 5
+*fizzbuzz for multiples of both 3 and 5
+*Return: 0
+*/
+int main(void)
+{
+int i;
+char a[] = "Fizz";
+char b[] = "Buzz";
+char c[] = "FizzBuzz";
+for (i = 1; i <= 100; i++)
+{
+if ((i % 3 == 0) && (1 % 5 == 0))
+printf("%s ", c);
+else if (i % 3 == 0)
+printf("%s ", a);
+else if (i % 5 == 0)
+printf("%s ", b);
+else
+printf("%d ", i);
+}
+printf("\n");
+return (0);
+
+}
