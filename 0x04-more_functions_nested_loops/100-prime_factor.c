bb74e786bdbbe6d220de277de228b00881e0975f
Nana Yaa <nanayaabob@gmail.com>
2023-02-25 08:19:52 -0800

    The problem of distinguishing prime numbers from composite numbers

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main - prints largest prime factor
+*Return: void
+*/
+int main(void)
+{
+long n, c;
+while (c < (n / 2))
+{
+if ((n % 2) == 0)
+{
+n /= 2;
+continue;
+}
+for (c = 3; c < (n / 2); c += 2)
+{
+if ((n % c) == 0)
+n /= c;
+}
+}
+printf("%ld\n", n);
+return (0);
+}
