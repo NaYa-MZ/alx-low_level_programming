a9079f4a4bf21735b8ead0bee5d114d566f080ca
Nana Yaa <nanayaabob@gmail.com>
2023-03-03 06:23:09 -0800

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,25 +1,25 @@
 #include "main.h"
-
 /**
- * print_number - prints an integer;
- * @n: integer to be printed;
+ * rot13 - funx that encodes a string using rot13
+ * @s: string to encode
+ * Return: s
  */
-void print_number(int n)
+char *rot13(char *s)
 {
-unsigned int n1;
+	int i, j;
+	char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
-	if (n < 0)
+	for (i = 0; *(s + i); i++)
 	{
-	n1 = -n;
-	_putchar('-');
+		for (j = 0; j < 52; j++)
+		{
+			if (a[j] == *(s + i))
+			{
+				*(s + i) = b[j];
+				break;
+			}
+		}
 	}
-	else
-	{
-	n1 = n;
-	}
-
-if (n1 / 10)
-print_number(n1 / 10);
-
-_putchar((n1 % 10) + '0');
+	return (s);
 }
